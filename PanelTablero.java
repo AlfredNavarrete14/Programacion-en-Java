912f3189d07037befd0964021f206e79883e4996
AlfredNavarrete14 <105526894+AlfredNavarrete14@users.noreply.github.com>
2022-05-13 19:52:59 -0500

    Create PanelTablero.java

---
+++ PanelTablero.java
@@ -0,0 +1,16 @@
+package com.usc.UI;
+
+import javax.swing.*;
+
+public class PanelTablero extends JPanel {
+
+    JLabel etiquetaPrueba;
+
+    public PanelTablero(){
+
+        etiquetaPrueba = new JLabel("Aqui va el tablero");
+        this.add(etiquetaPrueba);
+
+    }
+
+}
