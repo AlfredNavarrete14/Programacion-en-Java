ba4827a8865fc2faeec740c86f6f61e47138bb87
AlfredNavarrete14 <105526894+AlfredNavarrete14@users.noreply.github.com>
2022-05-13 19:52:12 -0500

    Create PanelLogo.java

---
+++ PanelLogo.java
@@ -0,0 +1,19 @@
+package com.usc.UI;
+
+import javax.swing.*;
+
+public class PanelLogo extends JPanel {
+
+    JLabel textoLogo;
+
+    public PanelLogo(){
+
+        textoLogo = new JLabel("Aqui va el logo");
+        textoLogo.setText("Aqui va el logo");
+        this.add(textoLogo);
+
+
+
+    }
+
+}
